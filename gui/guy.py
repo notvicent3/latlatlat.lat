aa6b3c38d1a8d516f4713d2147172bc66efd4500
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:37:45 +0200

    Update guy.py
    import tkinter as tk
    from tkinter import ttk
    
    class GUI:
        def __init__(self):
            self.root = tk.Tk()
            self.root.title("LATLATLAT.LAT")
            
            # Agrega elementos a la GUI aquí
    
        def run(self):
            self.root.mainloop()
    
        # Agrega más métodos según sea necesario

--- gui/guy.py
+++ gui/guy.py
@@ -1,1 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
 
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("LATLATLAT.LAT")
+        
+        # Agrega elementos a la GUI aquí
+
+    def run(self):
+        self.root.mainloop()
+
+    # Agrega más métodos según sea necesario
