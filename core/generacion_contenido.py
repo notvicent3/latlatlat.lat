f2b6e86e3ebbe4efdc47a2662451c6405da36f2d
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:56:39 +0200

    Update generacion_contenido.py
     def __init__(self):
            self.openai = OpenAIIntegration()
    
        def generar_resumen(self, data):
            prompt = f"Resumen de los datos recolectados: {data}"
            return self.openai.generate_text(prompt)

--- core/generacion_contenido.py
+++ core/generacion_contenido.py
@@ -7,3 +7,14 @@ class GeneracionContenido:
     def generar_resumen(self, data):
         prompt = f"Resumen de los datos recolectados: {data}"
         return self.openai.generate_text(prompt)
+
+    def generar_informe(self, datos, tipo_informe):
+        if tipo_informe == "resumen":
+            prompt = f"Genera un resumen basado en los siguientes datos: {datos}"
+        elif tipo_informe == "analisis":
+            prompt = f"Realiza un análisis detallado de los siguientes datos: {datos}"
+        else:
+            prompt = f"Descripción de los siguientes datos: {datos}"
+        
+        return self.openai.generate_text(prompt)
+
