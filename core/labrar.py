13ca888375a205b965615d53fe34c37a7704060b
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:38:15 +0200

    Update labrar.py

--- core/labrar.py
+++ core/labrar.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+# Importa otras librerías necesarias
+
+class Labrar:
+    def despliega_agentes(self):
+        # Implementa la lógica aquí
+        pass
+
+    # Agrega más métodos según sea necesario
 
