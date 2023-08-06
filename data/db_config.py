bb83b4c05bca00ff0ba4f30cc5b3038995b73a07
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:38:42 +0200

    Update db_config.py

--- data/db_config.py
+++ data/db_config.py
@@ -1,1 +1,7 @@
+from sqlalchemy import create_engine
+
+# Configuración de la base de datos
+DATABASE_URL = "postgresql://username:password@localhost:5432/mydatabase"
+
+engine = create_engine(DATABASE_URL)
 
