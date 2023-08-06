5ee025709ebf81e766410337b95bb166b684cae4
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:46:41 +0200

    Update labrar.py
    from bs4 import BeautifulSoup
    import requests
    # Importa las librerías necesarias para 2Captcha o soluciones similares si decides implementar esta funcionalidad.
    
    class Labrar:
        def despliega_agentes(self, url):
            response = requests.get(url)
            # Aquí puedes añadir una lógica para manejar captchas usando 2Captcha o soluciones similares.
            
            soup = BeautifulSoup(response.text, 'html.parser')
            # Continúa con el scraping adaptativo

--- core/labrar.py
+++ core/labrar.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+# Importa las librerías necesarias para 2Captcha o soluciones similares si decides implementar esta funcionalidad.
 
 class Labrar:
     def despliega_agentes(self, url):
         response = requests.get(url)
+        # Aquí puedes añadir una lógica para manejar captchas usando 2Captcha o soluciones similares.
+        
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Un ejemplo simple, extrae todos los párrafos de la página
-        paragraphs = soup.find_all('p')
-        return [p.text for p in paragraphs]
+        # Continúa con el scraping adaptativo
