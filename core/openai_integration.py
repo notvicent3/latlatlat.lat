77ef4d0608fd1ccdb75b6d0889b9c3f2f44ed1f6
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:53:58 +0200

    Create openai_integration.py
    import openai
    
    class OpenAIIntegration:
        def __init__(self):
            # Configura la clave de API
            openai.api_key = "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"
            
        def generate_text(self, prompt_text):
            # Asegúrate de no superar el límite de 8k tokens
            response = openai.Completion.create(
                model="gpt-3.5-turbo-16k",
                prompt=prompt_text,
                max_tokens=8000
            )
            return response.choices[0].text.strip()
    
        # Puedes añadir más métodos para otras funcionalidades relacionadas con OpenAI

---
+++ core/openai_integration.py
@@ -0,0 +1,17 @@
+import openai
+
+class OpenAIIntegration:
+    def __init__(self):
+        # Configura la clave de API
+        openai.api_key = "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"
+        
+    def generate_text(self, prompt_text):
+        # Asegúrate de no superar el límite de 8k tokens
+        response = openai.Completion.create(
+            model="gpt-3.5-turbo-16k",
+            prompt=prompt_text,
+            max_tokens=8000
+        )
+        return response.choices[0].text.strip()
+
+    # Puedes añadir más métodos para otras funcionalidades relacionadas con OpenAI
