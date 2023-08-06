9183bf17268d661b9cda7a61d447c9c7ea848299
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:57:34 +0200

    Update analisis_texto.py
    def categorizar_comentario(self, comentario):
            prompt = f"Categoriza el siguiente comentario: {comentario}"
            return self.openai.generate_text(prompt)
        
        def analizar_sentimiento(self, comentario):
            prompt = f"Analiza el sentimiento del siguiente comentario: {comentario}"
            return self.openai.generate_text(prompt)

--- core/analisis_texto.py
+++ core/analisis_texto.py
@@ -7,3 +7,11 @@ class AnalisisTexto:
     def interpretar_comentario(self, comentario):
         prompt = f"Interpretación del siguiente comentario: {comentario}"
         return self.openai.generate_text(prompt)
+        
+    def categorizar_comentario(self, comentario):
+        prompt = f"Categoriza el siguiente comentario: {comentario}"
+        return self.openai.generate_text(prompt)
+    
+    def analizar_sentimiento(self, comentario):
+        prompt = f"Analiza el sentimiento del siguiente comentario: {comentario}"
+        return self.openai.generate_text(prompt)
