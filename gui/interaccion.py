f4ee3eaf56ee18edab4d6da222ccf5e43ec6891f
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:57:07 +0200

    Update interaccion.py
    def __init__(self):
            self.openai = OpenAIIntegration()
    
        def respuesta_chatbot(self, pregunta_usuario):
            prompt = f"Usuario: {pregunta_usuario}\nAsistente:"
            return self.openai.generate_text(prompt)

--- gui/interaccion.py
+++ gui/interaccion.py
@@ -1,9 +1,14 @@
 from .openai_integration import OpenAIIntegration
 
 class Interaccion:
-    def __init__(self):
+   def __init__(self):
         self.openai = OpenAIIntegration()
 
     def respuesta_chatbot(self, pregunta_usuario):
         prompt = f"Usuario: {pregunta_usuario}\nAsistente:"
         return self.openai.generate_text(prompt)
+
+    def respuesta_chatbot_contextual(self, conversacion_previa, pregunta_usuario):
+        prompt = f"{conversacion_previa}\nUsuario: {pregunta_usuario}\nAsistente:"
+        return self.openai.generate_text(prompt)
+
