6bff988566c56f9e09c7cfa35497dc372a8d9b33
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:39:03 +0200

    Update automl.py
    from sklearn.model_selection import train_test_split
    import autokeras as ak
    
    class AutoML:
        def train(self, data):
            # Implementa la lógica de entrenamiento aquí
            pass
    
        # Agrega más métodos según sea necesario

--- automl/automl.py
+++ automl/automl.py
@@ -1,1 +1,10 @@
+from sklearn.model_selection import train_test_split
+import autokeras as ak
+
+class AutoML:
+    def train(self, data):
+        # Implementa la lógica de entrenamiento aquí
+        pass
+
+    # Agrega más métodos según sea necesario
 
