ff922f09149f4d098bcc4f03df94a36cada1807f
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:40:29 +0200

    Update guy.py
    import tkinter as tk
    from tkinter import ttk, Menu, Text, Scrollbar
    
    class GUI:
        def __init__(self):
            self.root = tk.Tk()
            self.root.title("LATLATLAT.LAT")
    
            # Menú
            menubar = Menu(self.root)
            self.root.config(menu=menubar)
            file_menu = Menu(menubar, tearoff=0)
            file_menu.add_command(label="Exit", command=self.root.quit)
            menubar.add_cascade(label="File", menu=file_menu)
    
            # Área de texto para mostrar resultados
            self.text_area = Text(self.root, wrap="word", width=50, height=15)
            scroll_bar = Scrollbar(self.root, command=self.text_area.yview)
            self.text_area.configure(yscrollcommand=scroll_bar.set)
            self.text_area.pack(padx=10, pady=10)
            scroll_bar.pack(side="right", fill="y")
    
        def run(self):
            self.root.mainloop()
    
        def update_text(self, new_text):
            self.text_area.insert(tk.END, new_text)

--- gui/guy.py
+++ gui/guy.py
@@ -1,14 +1,27 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Menu, Text, Scrollbar
 
 class GUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("LATLATLAT.LAT")
-        
-        # Agrega elementos a la GUI aquí
+
+        # Menú
+        menubar = Menu(self.root)
+        self.root.config(menu=menubar)
+        file_menu = Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Exit", command=self.root.quit)
+        menubar.add_cascade(label="File", menu=file_menu)
+
+        # Área de texto para mostrar resultados
+        self.text_area = Text(self.root, wrap="word", width=50, height=15)
+        scroll_bar = Scrollbar(self.root, command=self.text_area.yview)
+        self.text_area.configure(yscrollcommand=scroll_bar.set)
+        self.text_area.pack(padx=10, pady=10)
+        scroll_bar.pack(side="right", fill="y")
 
     def run(self):
         self.root.mainloop()
 
-    # Agrega más métodos según sea necesario
+    def update_text(self, new_text):
+        self.text_area.insert(tk.END, new_text)
