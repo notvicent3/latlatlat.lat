81dd7506aae22e9dedbee6753f0d3fd36f49ca92
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:43:07 +0200

    Update db_config.py
    from sqlalchemy import create_engine, Column, Integer, String, MetaData
    from sqlalchemy.ext.declarative import declarative_base
    from sqlalchemy.orm import sessionmaker
    
    Base = declarative_base()
    
    class DataEntry(Base):
        __tablename__ = 'data_entries'
        id = Column(Integer, primary_key=True)
        content = Column(String)
    
    DATABASE_URL = "postgresql://username:password@localhost:5432/mydatabase"
    engine = create_engine(DATABASE_URL)
    Session = sessionmaker(bind=engine)
    
    def init_db():
        Base.metadata.create_all(engine)

--- data/db_config.py
+++ data/db_config.py
@@ -1,7 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Configuración de la base de datos
+Base = declarative_base()
+
+class DataEntry(Base):
+    __tablename__ = 'data_entries'
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+
 DATABASE_URL = "postgresql://username:password@localhost:5432/mydatabase"
-
 engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
 
