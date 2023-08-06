da43cb90b03cf7b486fc19f57563d42f4659ecfd
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:49:28 +0200

    Update labrar.py
    from bs4 import BeautifulSoup
    import requests
    
    class Labrar:
        def __init__(self):
            self.headers = {
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
            }
    
        def despliega_agentes(self, url):
            response = requests.get(url, headers=self.headers)
    
            if response.status_code != 200:
                print(f"Error: {response.status_code}. No se pudo acceder al sitio.")
                return None
    
            soup = BeautifulSoup(response.text, 'html.parser')
            # Continúa con el scraping adaptativo
            # Por ejemplo, podríamos buscar ciertos elementos comunes en las páginas para determinar la estructura
            if soup.find('article'):
                # Lógica para sitios que usan el tag <article>
                pass
            elif soup.find('div', class_='post'):
                # Lógica para sitios que usan <div class="post">
                pass
            # ... y así sucesivamente para adaptarse a diferentes estructuras.

--- core/labrar.py
+++ core/labrar.py
@@ -1,11 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
-# Importa las librerías necesarias para 2Captcha o soluciones similares si decides implementar esta funcionalidad.
 
 class Labrar:
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+
     def despliega_agentes(self, url):
-        response = requests.get(url)
-        # Aquí puedes añadir una lógica para manejar captchas usando 2Captcha o soluciones similares.
-        
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}. No se pudo acceder al sitio.")
+            return None
+
         soup = BeautifulSoup(response.text, 'html.parser')
         # Continúa con el scraping adaptativo
+        # Por ejemplo, podríamos buscar ciertos elementos comunes en las páginas para determinar la estructura
+        if soup.find('article'):
+            # Lógica para sitios que usan el tag <article>
+            pass
+        elif soup.find('div', class_='post'):
+            # Lógica para sitios que usan <div class="post">
+            pass
+        # ... y así sucesivamente para adaptarse a diferentes estructuras.
