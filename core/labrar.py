3c92f739ce62ad6014bd61c646ec4f352e380f6e
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:40:48 +0200

    Update labrar.py
    from bs4 import BeautifulSoup
    import requests
    
    class Labrar:
        def despliega_agentes(self, url):
            response = requests.get(url)
            soup = BeautifulSoup(response.text, 'html.parser')
            # Un ejemplo simple, extrae todos los párrafos de la página
            paragraphs = soup.find_all('p')
            return [p.text for p in paragraphs]

--- core/labrar.py
+++ core/labrar.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-# Importa otras librerías necesarias
 
 class Labrar:
-    def despliega_agentes(self):
-        # Implementa la lógica aquí
-        pass
-
-    # Agrega más métodos según sea necesario
-
+    def despliega_agentes(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Un ejemplo simple, extrae todos los párrafos de la página
+        paragraphs = soup.find_all('p')
+        return [p.text for p in paragraphs]
