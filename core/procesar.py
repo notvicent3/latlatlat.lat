66a494012027ead2259925c23aea4a3de77b88b0
kerberos <132854638+kerberosai@users.noreply.github.com>
2023-08-06 06:46:58 +0200

    Update procesar.py
    import pandas as pd
    from textblob import TextBlob  # Para análisis de sentimiento
    
    class Procesar:
        def limpia_datos(self, data_frame):
            # Limpieza básica de datos usando pandas
            df_clean = data_frame.dropna()  # Elimina filas con valores NaN
            # Agrega más lógica de limpieza según sea necesario
            
            return df_clean
        
        def analisis_sentimiento(self, texto):
            analysis = TextBlob(texto)
            return analysis.sentiment.polarity

--- core/procesar.py
+++ core/procesar.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from textblob import TextBlob  # Para análisis de sentimiento
 
+class Procesar:
+    def limpia_datos(self, data_frame):
+        # Limpieza básica de datos usando pandas
+        df_clean = data_frame.dropna()  # Elimina filas con valores NaN
+        # Agrega más lógica de limpieza según sea necesario
+        
+        return df_clean
+    
+    def analisis_sentimiento(self, texto):
+        analysis = TextBlob(texto)
+        return analysis.sentiment.polarity
